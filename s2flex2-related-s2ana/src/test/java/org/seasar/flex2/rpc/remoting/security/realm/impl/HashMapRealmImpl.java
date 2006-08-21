391aaf759fc921a862d321d3b15708525f1d9724
e1arkw <e1arkw@881adaa7-5f29-445a-92c6-36a57acc6958>
2006-08-21 01:43:58 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/akabana/trunk@14 881adaa7-5f29-445a-92c6-36a57acc6958

--- s2flex2-related-s2ana/src/test/java/org/seasar/flex2/rpc/remoting/security/realm/impl/HashMapRealmImpl.java
+++ s2flex2-related-s2ana/src/test/java/org/seasar/flex2/rpc/remoting/security/realm/impl/HashMapRealmImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.seasar.flex2.rpc.remoting.security.realm.impl;
 
 import java.util.Map;
@@ -11,10 +26,10 @@ public class HashMapRealmImpl extends AbstractRemotingServiceRealmImpl {
 
     private Map userRoleMap;
 
-    public RemotingServicePrincipal authenticate(final String userid, final String password) {
+    public RemotingServicePrincipal authenticate(final String name, final String password) {
         RemotingServicePrincipal principal = null;
-        if (password.equalsIgnoreCase(getPassword(userid))) {
-            principal = createPrincipal(userid,principal.getRole());
+        if (password.equalsIgnoreCase(getPassword(name))) {
+            principal = createPrincipal(name,getRole(name));
         }
         return principal;
     }
@@ -22,7 +37,7 @@ public class HashMapRealmImpl extends AbstractRemotingServiceRealmImpl {
     public boolean hasRole(final RemotingServicePrincipal principal, final String role) {
         boolean hasRole = false;
         if (userRoleMap.containsKey(principal.getName())) {
-            hasRole = role.equalsIgnoreCase(getRole(principal));
+            hasRole = role.equalsIgnoreCase(getRole(principal.getName()));
         }
         return hasRole;
     }
@@ -39,7 +54,7 @@ public class HashMapRealmImpl extends AbstractRemotingServiceRealmImpl {
         return (String) userMap.get(userid);
     }
 
-    protected String getRole(RemotingServicePrincipal principal) {
-        return (String) userRoleMap.get(principal.getName());
+    protected String getRole(String name) {
+        return (String) userRoleMap.get(name);
     }
 }
