eec074023aa46d3d0dfed3cfd959e7b67ecd58f3
nod <nod@881adaa7-5f29-445a-92c6-36a57acc6958>
2006-09-06 12:46:18 +0000

    javadoc comment deleted.
    git-svn-id: https://www.seasar.org/svn/sandbox/akabana/trunk@49 881adaa7-5f29-445a-92c6-36a57acc6958

--- fds-s2factory/src/main/java/org/seasar/flex2/rpc/remoting/service/fds2/factory/Seasar2Factory.java
+++ fds-s2factory/src/main/java/org/seasar/flex2/rpc/remoting/service/fds2/factory/Seasar2Factory.java
@@ -37,16 +37,6 @@ public class Seasar2Factory extends RemotingServiceInvokerImpl implements
 	 * This method is called when the definition of an instance that this factory
 	 * looks up is initialized.
 	 */
-	/*
-	 * This method is called when we initialize the definition of an instance 
-	 * which will be looked up by this factory.  It should validate that
-	 * the properties supplied are valid to define an instance.
-	 * Any valid properties used for this configuration must be accessed to 
-	 * avoid warnings about unused configuration elements.  If your factory 
-	 * is only used for application scoped components, this method can simply
-	 * return a factory instance which delegates the creation of the component
-	 * to the FactoryInstance's lookup method.
-	 */
 	public FactoryInstance createFactoryInstance(String id, ConfigMap properties) {
 
 		
@@ -61,16 +51,7 @@ public class Seasar2Factory extends RemotingServiceInvokerImpl implements
 
 	/**
 	 * Returns the instance specified by the source
-	 * and properties arguments.  For the factory, this may mean
-	 * constructing a new instance, optionally registering it in some other
-	 * name space such as the session or JNDI, and then returning it
-	 * or it may mean creating a new instance and returning it.
-	 * This method is called for each request to operate on the
-	 * given item by the system so it should be relatively efficient.
-	 * <p>
-	 * If your factory does not support the scope property, it
-	 * report an error if scope is supplied in the properties
-	 * for this instance.
+	 * and properties arguments. 
 	 */
 	
 	public Object lookup(FactoryInstance factoryInstance) {
