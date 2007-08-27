5c03289c184f160e8ec94bbd4858d0923b1c5c0b
nod <nod@881adaa7-5f29-445a-92c6-36a57acc6958>
2007-08-27 18:19:00 +0000

    new vesion file
    git-svn-id: https://www.seasar.org/svn/sandbox/akabana/trunk@315 881adaa7-5f29-445a-92c6-36a57acc6958

---
+++ s2flex2-ana-example/src/main/java/org/seasar/flex2/rpc/remoting/security/realm/impl/HashMapRealmImpl.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.flex2.rpc.remoting.security.realm.impl;
+
+import java.util.Map;
+
+import org.seasar.flex2.rpc.remoting.security.RemotingServicePrincipal;
+import org.seasar.flex2.rpc.remoting.security.realm.RemotingServiceRealm;
+
+public class HashMapRealmImpl implements RemotingServiceRealm {
+    
+    private Map userMap;
+    
+    private Map userRoleMap;
+    
+    public boolean authenticate(final String name, final String password) {
+        return password.equals(getPassword(name));
+    }
+    
+    public boolean hasRole(final RemotingServicePrincipal principal,
+            final String role) {
+        boolean hasRole = false;
+        if (userRoleMap.containsKey(principal.getName())) {
+            hasRole = role.equalsIgnoreCase(getRole(principal.getName()));
+        }
+        return hasRole;
+    }
+    
+    public void setUserMap(Map userMap) {
+        this.userMap = userMap;
+    }
+    
+    public void setUserRoleMap(Map userRoleMap) {
+        this.userRoleMap = userRoleMap;
+    }
+    
+    protected String getPassword(String userid) {
+        return (String) userMap.get(userid);
+    }
+    
+    public String getRole(String name) {
+        return (String) userRoleMap.get(name);
+    }
+}
