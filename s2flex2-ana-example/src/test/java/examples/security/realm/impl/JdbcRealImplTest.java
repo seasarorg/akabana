8ab61871556f9200dc88bae97f455504ea31b0f8
nod <nod@881adaa7-5f29-445a-92c6-36a57acc6958>
2007-09-02 16:13:30 +0000

    JdbcRealmImpl Testcase and essential files.
    git-svn-id: https://www.seasar.org/svn/sandbox/akabana/trunk@326 881adaa7-5f29-445a-92c6-36a57acc6958

---
+++ s2flex2-ana-example/src/test/java/examples/security/realm/impl/JdbcRealImplTest.java
@@ -0,0 +1,35 @@
+package examples.security.realm.impl;
+
+import org.seasar.extension.unit.S2TestCase;
+import org.seasar.flex2.rpc.remoting.security.RemotingServicePrincipal;
+import org.seasar.flex2.rpc.remoting.security.realm.RemotingServiceRealm;
+
+public class JdbcRealImplTest extends S2TestCase {
+	
+	    private final static String PATH = "s2flex2-ana-realm-test.dicon";
+	    
+	    private RemotingServiceRealm remotingServiceRealm;
+	    
+	    private RemotingServicePrincipal remotingServicePricipal;
+	    
+	    protected void setUp() throws Exception {
+	        include(PATH);
+	    }
+	    public void testRealmAuthenticated(){
+	    	assertNotNull("realm implementation is not null",remotingServiceRealm);
+	    	assertTrue("realm Implementation is JdbcRealm",remotingServiceRealm instanceof RemotingServiceRealm);
+	    	assertTrue("realmIsAuthenticated",remotingServiceRealm.authenticate("admin", "adminpassword"));
+	    	
+	    }
+	    public void testHasRole(){
+	    	assertNotNull("realm implementation is not null",remotingServiceRealm);
+	    	assertTrue("hasRole",remotingServiceRealm.hasRole(remotingServicePricipal, "admin"));
+	    	assertTrue("hasRole",remotingServiceRealm.hasRole(remotingServicePricipal, "manager"));
+
+	    }
+	    
+	    public void testGetRole(){
+	    	assertSame("Test getRole ",remotingServiceRealm.getRole("admin") ,"admin");
+	    	assertNotSame("Test getRole always returns [admin]",remotingServiceRealm.getRole("admin") ,"manager");
+	    }
+}
