edbe433c2070ddd4ec7cb89e16f8abd63c188417
nod <nod@881adaa7-5f29-445a-92c6-36a57acc6958>
2007-09-02 16:08:42 +0000

    source format
    git-svn-id: https://www.seasar.org/svn/sandbox/akabana/trunk@324 881adaa7-5f29-445a-92c6-36a57acc6958

--- s2flex2-ana-example/src/main/java/examples/flex2/add/service/impl/AddServiceImpl.java
+++ s2flex2-ana-example/src/main/java/examples/flex2/add/service/impl/AddServiceImpl.java
@@ -16,35 +16,38 @@ import examples.flex2.add.service.AddService;
 public class AddServiceImpl implements AddService {
 
 	private static final long serialVersionUID = -3992983806172401104L;
+
 	private AddDto addDto;
-	
-    public int calculate(int arg1, int arg2) {
-        return arg1 + arg2;
-    }
 
-    public AddDto calculate2(AddDto addDto) {
-    	addDto.setArg1(addDto.getArg1());
-    	addDto.setArg2(addDto.getArg2());
-        addDto.setSum(addDto.getArg1() + addDto.getArg2());
-        this.addDto=addDto;
-        this.addDto.setCalclateDate(new Date());
-        return this.addDto;
-    }
-    
-    public AddDto getAddDtoData(){
-    	if(this.addDto != null){
-    		return this.addDto;
-    	}
-    	this.addDto = new AddDto();
-    	return this.addDto;
-    }
+	public int calculate(int arg1, int arg2) {
+		return arg1 + arg2;
+	}
 
-    @Export(storage = StorageType.SESSION)
+	public AddDto calculate2(AddDto addDto) {
+		addDto.setArg1(addDto.getArg1());
+		addDto.setArg2(addDto.getArg2());
+		addDto.setSum(addDto.getArg1() + addDto.getArg2());
+		this.addDto = addDto;
+		this.addDto.setCalclateDate(new Date());
+		return this.addDto;
+	}
+
+	public AddDto getAddDtoData() {
+		if (this.addDto != null) {
+			return this.addDto;
+		}
+		this.addDto = new AddDto();
+		return this.addDto;
+	}
+
+	@Export(storage = StorageType.SESSION)
 	public AddDto getAddDto() {
 		return addDto;
 	}
-    @Import(storage = StorageType.SESSION)
+
+	@Import(storage = StorageType.SESSION)
 	public void setAddDto(AddDto addDto) {
 		this.addDto = addDto;
 	}
+
 }
