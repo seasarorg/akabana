cee5ea14fd609f10ddd95bf3da978e972dab8d17
e1arkw <e1arkw@881adaa7-5f29-445a-92c6-36a57acc6958>
2012-02-20 02:49:44 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/akabana/trunk@1724 881adaa7-5f29-445a-92c6-36a57acc6958

--- yui-examples/flex3/yui-s2flex2-example/dolteng/src/main/java/root/yui/example/service/AbstractService.java
+++
@@ -1,7 +0,0 @@
-package root.yui.example.service;
-
-import org.seasar.extension.jdbc.service.S2AbstractService;
-
-public abstract class AbstractService<ENTITY> extends S2AbstractService<ENTITY> {
-
-}
